54ed0017e11152375968da97b6433f54c3e1c9ff
Dushan Saputhanthri <ryracircuit@gmail.com>
2021-12-23 15:33:07 +0530

    Initial commit with .gitignore

---
+++ Learn-SwiftUI/Learn-SwiftUI/Learn_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUIApp.swift
+//  Learn-SwiftUI
+//
+//  Created by Dushan Saputhanthri on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
